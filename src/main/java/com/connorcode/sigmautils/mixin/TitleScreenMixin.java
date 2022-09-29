dbce90b8dc6052fdca65d54b14dfbbf9374132af
Connor Slade <connor@connorcode.com>
2022-09-28 23:03:53 -0400

    Splash Refresh Module

--- src/main/java/com/connorcode/sigmautils/mixin/TitleScreenMixin.java
+++ src/main/java/com/connorcode/sigmautils/mixin/TitleScreenMixin.java
@@ -3,14 +3,18 @@ package com.connorcode.sigmautils.mixin;
 import com.connorcode.sigmautils.config.Config;
 import com.connorcode.sigmautils.config.ConfigGui;
 import com.connorcode.sigmautils.misc.Util;
+import com.connorcode.sigmautils.modules._interface.SplashRefresh;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Language;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -19,11 +23,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class TitleScreenMixin extends Screen {
+    long updateTime;
+    @Shadow
+    @Nullable
+    private String splashText;
+
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+
+    // Always Enabled
     @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;"))
     void init(CallbackInfo ci) {
         ScreenAccessor screen = ((ScreenAccessor) this);
-        Util.addDrawable((Screen) (Object) this,
+        Util.addDrawable(this,
                 new ButtonWidget(screen.getWidth() / 2 - 100 - 24, screen.getHeight() / 4 + 48 + 24, 20, 20,
                         Text.of("Σ"), button -> MinecraftClient.getInstance()
                         .setScreen(new ConfigGui()),
@@ -32,6 +46,20 @@ public class TitleScreenMixin {
                                         .reorder(List.of(Text.of("Sigma Utils"))), mouseX, mouseY))));
     }
 
+
+    // For splash_refresh
+    @Inject(method = "render", at = @At("HEAD"))
+    void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        long now = System.currentTimeMillis();
+        if (!Config.getEnabled(SplashRefresh.class) || now - updateTime < SplashRefresh.refreshTime) return;
+
+        updateTime = now;
+        assert this.client != null;
+        this.splashText = this.client.getSplashTextLoader()
+                .get();
+    }
+
+    // For minceraft
     @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/TitleScreen;isMinceraft:Z", opcode = Opcodes.GETFIELD))
     boolean isMinecraft(TitleScreen instance) {
         return Config.getEnabled("minceraft");
--- src/main/java/com/connorcode/sigmautils/modules/chat/NoMessageHiding.java
+++ src/main/java/com/connorcode/sigmautils/modules/chat/NoMessageHiding.java
@@ -5,6 +5,7 @@ import com.connorcode.sigmautils.module.Category;
 
 public class NoMessageHiding extends BasicModule {
     public NoMessageHiding() {
-        super("no_message_hiding", "No Message Hiding", "Removes the ability of the server to hide previously sent chat messages.", Category.Chat);
+        super("no_message_hiding", "No Message Hiding",
+                "Removes the ability of the server to hide previously sent chat messages.", Category.Chat);
     }
 }
