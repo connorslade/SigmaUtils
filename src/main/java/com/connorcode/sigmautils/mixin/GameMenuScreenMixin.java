7b74aae59c81d690083e3d37d821c0afe858949c
Connor Slade <connor@connorcode.com>
2022-11-13 14:51:22 -0500

    ConfirmDisconnect module

---
+++ src/main/java/com/connorcode/sigmautils/mixin/GameMenuScreenMixin.java
@@ -0,0 +1,33 @@
+package com.connorcode.sigmautils.mixin;
+
+import com.connorcode.sigmautils.config.Config;
+import com.connorcode.sigmautils.modules._interface.ConfirmDisconnect;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(GameMenuScreen.class)
+public class GameMenuScreenMixin extends Screen {
+    protected GameMenuScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "initWidgets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIIILnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)V", ordinal = 8, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
+    void onDisconnectButton(CallbackInfo ci, int i, int j, String string, ButtonWidget buttonWidget, Text text) {
+        if (!Config.getEnabled(ConfirmDisconnect.class)) return;
+        ci.cancel();
+
+        this.addDrawableChild(
+                new ButtonWidget(this.width / 2 - 102, this.height / 4 + 120 + -16, 204, 20, text, (button) -> {
+                    button.active = false;
+                    assert this.client != null;
+                    this.client.setScreen(new ConfirmDisconnect.ConfirmDisconnectScreen());
+                }));
+    }
+}
---
+++ src/main/java/com/connorcode/sigmautils/modules/_interface/ConfirmDisconnect.java
@@ -0,0 +1,63 @@
+package com.connorcode.sigmautils.modules._interface;
+
+import com.connorcode.sigmautils.module.BasicModule;
+import com.connorcode.sigmautils.module.Category;
+import net.minecraft.client.gui.screen.MessageScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+public class ConfirmDisconnect extends BasicModule {
+    public ConfirmDisconnect() {
+        super("confirm_disconnect", "Confirm Disconnect", "Requires you to confirm your server disconnects.",
+                Category.Interface);
+    }
+
+    public static class ConfirmDisconnectScreen extends Screen {
+        public ConfirmDisconnectScreen() {
+            super(Text.of("Disconnect?"));
+        }
+
+        @Override
+        protected void init() {
+            this.addDrawableChild(
+                    new ButtonWidget(this.width / 2 - 122, this.height / 2 - 10, 120, 20, Text.of("Disconnect"),
+                            (button -> {
+                                assert this.client != null;
+                                assert this.client.world != null;
+                                boolean singlePlayer = client.isInSingleplayer();
+                                boolean realms = client.isConnectedToRealms();
+
+                                client.world.disconnect();
+                                if (singlePlayer)
+                                    client.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
+                                else client.disconnect();
+
+                                TitleScreen titleScreen = new TitleScreen();
+                                if (singlePlayer) client.setScreen(titleScreen);
+                                else if (realms) client.setScreen(new RealmsMainScreen(titleScreen));
+                                else client.setScreen(new MultiplayerScreen(titleScreen));
+                            })));
+
+            this.addDrawableChild(
+                    new ButtonWidget(this.width / 2 + 2, this.height / 2 - 10, 120, 20, Text.of("Back"), (button -> {
+                        assert client != null;
+                        client.setScreen(null);
+                    })));
+        }
+
+        @Override
+        public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+            assert client != null;
+            fillGradient(matrices, 0, 0, this.width, this.height, -1072689136, -804253680);
+            Text text = Text.of("Do you want to disconnect?");
+            client.textRenderer.draw(matrices, text, this.width / 2f - textRenderer.getWidth(text) / 2f,
+                    this.height / 2f - 20 - textRenderer.fontHeight / 2f, 16777215);
+            super.render(matrices, mouseX, mouseY, delta);
+        }
+    }
+}
