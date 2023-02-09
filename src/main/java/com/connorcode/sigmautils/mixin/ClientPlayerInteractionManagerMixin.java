c756ec1709542b0a7bd961a99963c8f404dc81a9
Connor Slade <connor@connorcode.com>
2023-02-09 17:30:54 -0500

    NoCooldown module

--- src/main/java/com/connorcode/sigmautils/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/com/connorcode/sigmautils/mixin/ClientPlayerInteractionManagerMixin.java
@@ -16,11 +16,19 @@ public class ClientPlayerInteractionManagerMixin {
     @Shadow
     private int blockBreakingCooldown;
 
+    // Idea from https://github.com/blackd/IPN-Rejects
     @Inject(method = "updateBlockBreakingProgress", at = @At("HEAD"), cancellable = true)
     void onUpdateBlockBreakingProgress(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
-        if (this.blockBreakingCooldown < 0 && Config.getEnabled(NoCooldown.class) && NoCooldown.noBlockBreak.value()) {
-            this.blockBreakingCooldown = 0;
-            cir.cancel();
-        }
+        if (this.blockBreakingCooldown <= 0 || !Config.getEnabled(NoCooldown.class) || !NoCooldown.noBlockBreak.value())
+            return;
+        this.blockBreakingCooldown = 0;
+        cir.cancel();
+    }
+
+    @Inject(method = "breakBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onBreak(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)V"))
+    void onBlockBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (this.blockBreakingCooldown <= 0 || !Config.getEnabled(NoCooldown.class) || !NoCooldown.noBlockBreak.value())
+            return;
+        this.blockBreakingCooldown = 0;
     }
 }
