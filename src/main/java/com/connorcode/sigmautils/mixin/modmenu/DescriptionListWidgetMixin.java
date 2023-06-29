bfc9dd92aefa35d68ecab4ff33055cf524f3032c
Connor Slade <connor@connorcode.com>
2023-06-29 02:10:35 -0400

    Don't crash on wrong Mod Menu version

--- src/main/java/com/connorcode/sigmautils/mixin/modmenu/DescriptionListWidgetMixin.java
+++ src/main/java/com/connorcode/sigmautils/mixin/modmenu/DescriptionListWidgetMixin.java
@@ -4,16 +4,19 @@ import com.connorcode.sigmautils.config.Config;
 import com.connorcode.sigmautils.modules._interface.RandomBackground;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.terraformersmc.modmenu.gui.widget.DescriptionListWidget;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = DescriptionListWidget.class, remap = false)
 public class DescriptionListWidgetMixin {
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V"))
-    private void render(int texture, Identifier id) {
-        if (Config.getEnabled(RandomBackground.class)) id = RandomBackground.getTexture();
-        RenderSystem.setShaderTexture(texture, id);
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V", shift = At.Shift.AFTER))
+    private void render(DrawContext DrawContext, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        var id = Config.getEnabled(RandomBackground.class) ?
+                RandomBackground.getTexture() : Screen.OPTIONS_BACKGROUND_TEXTURE;
+        RenderSystem.setShaderTexture(0, id);
     }
 }
