98b054b775db3aefb9de7b7f46708f82973692ac
Connor Slade <connor@connorcode.com>
2023-07-11 20:45:07 -0400

    Start on AutoTradeCycle

--- src/main/java/com/connorcode/sigmautils/modules/server/AutoTradeCycle.java
+++ src/main/java/com/connorcode/sigmautils/modules/server/AutoTradeCycle.java
@@ -1,9 +1,28 @@
 package com.connorcode.sigmautils.modules.server;
 
 import com.connorcode.sigmautils.config.ModuleInfo;
+import com.connorcode.sigmautils.config.settings.DynamicListSetting;
+import com.connorcode.sigmautils.config.settings.list.SimpleList;
 import com.connorcode.sigmautils.module.Module;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.Registries;
 
-@ModuleInfo(description = "Automatically cycles a villager's trades until you get the desired trade.")
+@ModuleInfo(description = "Automatically cycles a villager's trades until you get the desired trade. [EXPERIMENTAL]")
 public class AutoTradeCycle extends Module {
+    DynamicListSetting<Enchantment> enchantments =
+            new DynamicListSetting<>(AutoTradeCycle.class, "Enchantments", EnchantmentList::new)
+                    .description("The enchantments that will stop the cycling.")
+                    .build();
 
+    static class EnchantmentList extends SimpleList<Enchantment> {
+
+        public EnchantmentList(DynamicListSetting<Enchantment> setting) {
+            super(setting, Registries.ENCHANTMENT);
+        }
+
+        @Override
+        public String getDisplay(Enchantment resource) {
+            return resource.getName(1).getString();
+        }
+    }
 }
