d0541debfe08c2ca8555fff0d5da8f8261654926
Connor Slade <connor@connorcode.com>
2024-06-27 16:39:50 -0400

    Fail to fix custom payload handling

---
+++ src/main/java/com/connorcode/sigmautils/mixin/ClientCommonNetworkHandlerMixin.java
@@ -0,0 +1,29 @@
+package com.connorcode.sigmautils.mixin;
+
+import net.minecraft.client.network.ClientCommonNetworkHandler;
+import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(ClientCommonNetworkHandler.class)
+public class ClientCommonNetworkHandlerMixin {
+    @Inject(method = "onCustomPayload", at = @At("HEAD"))
+    void onCustomPayload(CustomPayloadS2CPacket packet) {
+//        var buf = packet.getPayload();
+//        var event = new UnknownPacketEvent(buf, packet.payload().getId().id());
+        System.out.println("CUSTOM PAYLOAD:" + packet.payload().getClass().getSimpleName());
+    }
+}
+
+
+//    @Inject(method = "readPayload", at = @At("HEAD"), cancellable = true)
+//    private static void onReadPayload(Identifier id, PacketByteBuf buf, CallbackInfoReturnable<CustomPayload> cir) {
+//        if (ID_TO_READER.containsKey(id)) return;
+//
+//        var event = new UnknownPacketEvent(buf, id);
+//        SigmaUtils.eventBus.post(event);
+//
+//        if (event.isCancelled()) cir.setReturnValue(new CancelledUnknownPacket(id));
+//        else cir.setReturnValue(readUnknownPayload(id, buf));
+//    }
